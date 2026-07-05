79c2a1540217071b0fa814c6eda80286ddcdfb79
Javid Ahmed <javidahmed@icloud.com>
2026-07-05 22:25:19 +0100

    Add delay route to Rocket web server for asynchronous waiting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #[macro_use]
 extern crate rocket;
+use rocket::tokio::time::{Duration, sleep};
 
 #[get("/hello/<name>")]
 fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, delay])
 }
